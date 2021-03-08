4cdbf9f6b0581bc02d84d693bcf44625ddb0010f
happyf1re <dmur1991@gmail.com>
2021-03-08 23:16:28 +0300

    1

--- Lesson7DZ/src/main/java/Cat.java
+++ Lesson7DZ/src/main/java/Cat.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+    String name;
+    int hunger;
+    boolean satiety;
+
+    public Cat(String name) {
+        this.name = name;
+        this.hunger = 5;
+        this.satiety = true;
+    }
+
+    public void catInfo(Cat cat) {
+        if (cat.hunger <= 0) {
+            cat.satiety = false;
+            System.out.println(cat.name + " больше не голодна!");
+        }
+        else {
+            System.out.println("У " + cat.name + " еще " + cat.hunger + " единиц голода!");
+        }
+
+
+    }
 }

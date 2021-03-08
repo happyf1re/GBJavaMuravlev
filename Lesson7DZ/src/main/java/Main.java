a19a7bf6de724b13291ffe03214afb0003ac6316
happyf1re <dmur1991@gmail.com>
2021-03-08 23:17:20 +0300

    2

--- Lesson7DZ/src/main/java/Main.java
+++ Lesson7DZ/src/main/java/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Cat Ryzh = new Cat ("Ryzh");
+        Cat Deni = new Cat ("Deni");
+        Cat Zulia = new Cat ("Zulia");
+
+
+        Plate plate = new Plate();
+
+        Cat [] cats = new Cat[3];
+
+        cats[0] = Ryzh;
+        cats[1] = Deni;
+        cats[2] = Zulia;
+
+        plate.fill(50);
+        plate.plateInfo();
+        System.out.println();
+        for (Cat cat : cats) {
+            plate.feed(cat, 5);
+            cat.catInfo(cat);
+        }
+        System.out.println();
+        plate.plateInfo();
+
+
+
+    }
 }
+

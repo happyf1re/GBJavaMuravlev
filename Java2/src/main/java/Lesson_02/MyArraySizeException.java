2df50db711e25b42336ff430c0a6188cc9ec36f7
happyf1re <78656949+happyf1re@users.noreply.github.com>
2021-03-21 13:36:22 +0300

    Delete Java2/src/main/java/Lesson_02 directory

--- Java2/src/main/java/Lesson_02/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_02;
-
-public class MyArraySizeException extends Exception{
-    public MyArraySizeException() {
-        System.err.println("Размер массива должен быть 4х4!");
-    }
-}
